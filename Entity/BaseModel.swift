79d6fdca4521cb9287ff4c90a9cd2f7550e3f9b5
伟哥 <564114648@qq.com>
2016-05-18 11:01:12 +0800

    //.

---
+++ Entity/BaseModel.swift
@@ -0,0 +1,21 @@
+//
+//  BaseModel.swift
+//  palmBusiness
+//
+//  Created by nevermore on 16/4/20.
+//  Copyright © 2016年 hzw. All rights reserved.
+//
+
+import UIKit
+import ObjectMapper
+/// 模型超类
+class BaseModel:Mappable{
+    
+    required init?(_ map: Map) {
+        
+    }
+    func mapping(map: Map) {
+        
+    }
+    
+}
